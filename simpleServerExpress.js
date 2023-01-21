cd95345d6c9998e8bc40bdf4e2e8863d56129dce
An Hoang <anvanhoang2014@gmail.com>
2023-01-21 17:30:24 -0500

    Simple server

--- simpleServerExpress.js
+++ simpleServerExpress.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
